f143963be70f6ca315de3703550b08a41fb27465
Steve Young <nocturnalsteve@gmail.com>
2016-11-17 07:34:33 +0000

    first commit

--- src/test/java/enums/Platform.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum Platform {
-	Windows, Windows10, Windows8_1, Windows8, Windows7, WindowsXP,
-	OSX, OSX10_11, OSX10_10, OSX10_9, OSX10_8, OSX10_7, OSX10_6,
-	Ubuntu15_04, Ubuntu14_10, Ubuntu14_04,
-	iOS,
-	Android
-}
